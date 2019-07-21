7d0e3b2542bc9e73879fb690a024df6b95a88c0f
lucasflomuller <lucasflorianomuller@gmail.com>
2019-07-21 04:30:58 +0000

    create initial passport routes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
+const passport = require("passport");
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("hello world")
-});
+// requiring routes
+const authRoutes = require("./routes/authRoutes");
+
+// passport initialization
+require("./services/passport");
+app.use(passport.initialize());
+
+// use routes
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ services/passport.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const User = require("../models/User");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
